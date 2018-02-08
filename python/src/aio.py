76def035ea4e5c66df668a3fde70b9a6af61124f
David Sun <iamslash@gmail.com>
2018-02-08 12:29:18 +0900

    refined src dir

--- python/src/aio.py
+++ python/src/aio.py
@@ -1,20 +1,5 @@
-# Abstract
+# -*- coding: utf-8 -*-
 
-python3에 대해 정리한다.
-
-# Usage
-
-## pdb
-
-pdb로 break point하고 싶은 라인에 다음을 복사하고 실행하자.
-
-```python
-import pdb; pdb.set_trace()
-```
-
-## async, await
-
-```python
 import aiohttp
 import asyncio
 import async_timeout
@@ -34,27 +19,3 @@ async def main():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-```
-
-
-# Library
-
-## regex
-
-```python
-import re
-p = re.compile(r'(?P<word>\b\w*\b)')
-m = p.search('(((( Lots of punctuation )))')
-print(m.group('word'))
-print(m.group(0))
-print(m.group(1))
-```
-
-## numpy
-
-## pandas
-
-# References
-
-* [파이썬 생존 안내서](https://www.slideshare.net/sublee/ss-67589513)
-  * 듀랑고를 제작한 왓스튜디오의 이흥섭 PT
